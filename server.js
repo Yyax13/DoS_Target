7efcf147048b848cfdc4faa87fa786a20add11ad
Yyax13 <Yyax13@proton.me>
2025-07-16 17:38:17 -0300

    [FIX] Fixed logging

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ import cors from 'cors';
 
 const app = e();
 app.use(cors());
+app.set('trust proxy', true);
 
 app.use((req, res) => {
     const ipFromHeader = req.headers['x-forwarded-for'];
-    let ip = req.connection.remoteAddress || req.socket.remoteAddress;
+    let ip = req.socket.remoteAddress || req.connection.remoteAddress;
     ip = ip.replace('::ffff:', '');
-    if (ip == '::1' || ip == '127.0.0.1') ip = '0.0.0.0';
 
-    console.log(`[${new Date().toISOString()}] - [${req.method}] | X-Fowarded-For: ${ipFromHeader ? ipFromHeader : null} | IP: ${ip}`);
+    console.table({
+        Timestamp: new Date().toISOString(),
+        Method: req.method,
+        'X-Fowarded-For': ipFromHeader ? ipFromHeader : null,
+        'remoteAddress IP': ip,
+        'Request IP': req.ip
+    })
     res.status(200).json({
         method: req.method,
         ip: ip, message: 'Hello',
@@ -21,7 +27,7 @@ app.use((req, res) => {
 
 });
 
-app.listen(28080, () => {
-    console.log('Listening on port 28080');
+app.listen(80, () => {
+    console.log('Listening on port 80');
 
 });
